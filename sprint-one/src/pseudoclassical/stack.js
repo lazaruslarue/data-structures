5927df54870d2a6470fc7a7f831923f9aa1a37bf
Hack Reactor Students <github@hackreactor.com>
2013-11-14 14:09:25 -0800

    completed pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,26 +1,15 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
-  //var instance = {};
-  var instance = Object.create(stackMethods);
-
-  instance.storage = {};
-  instance.length = 0; 
-
-  return instance;
+  this.storage = {};
+  this.length = 0;
 };
-
-var stackMethods = {
-  size: function(){
+Stack.prototype.size = function(){
     return this.length;
-  },
-
-  push: function(value){
+};
+Stack.prototype.push = function(value){
     this.storage[this.length++] = value;
-  },
-
-  pop: function(){
+};
+Stack.prototype.pop = function(){
     this.length && this.length--;
     return this.storage[this.length];
-  }
 };
 
