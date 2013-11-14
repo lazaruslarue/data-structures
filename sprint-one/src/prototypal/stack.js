65e97e800cdd984411b4c281c2a7a88f0adeff90
Hack Reactor Students <github@hackreactor.com>
2013-11-14 10:35:23 -0800

    copied code. added Object.create for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,32 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = {};
+  instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+
+  instance.push = stackMethods.push;
+  instance.size = stackMethods.size;
+  instance.pop = stackMethods.pop;
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+    size: function(){
+    return this.length;
+  },
+
+  push: function(value){
+    this.storage[this.length++] = value;
+  },
+
+  pop: function(){
+    this.length && this.length--;
+    return this.storage[this.length];
+  }
+};
