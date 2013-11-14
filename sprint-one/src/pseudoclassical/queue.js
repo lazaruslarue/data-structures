8660fa9f746d9dc16ef8e2b275bc24ce4081b2a5
Hack Reactor Students <github@hackreactor.com>
2013-11-14 11:04:38 -0800

    moved on to pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,41 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = new Queue ;
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.length = 0;
+  // Implement the methods below
+
+
+  // instance.size = queueMethods.size;
+  // instance.enqueue = queueMethods.enqueue;
+
+  // instance.dequeue = queueMethods.dequeue;
+
+
+  return instance;
+};
+
+var queueMethods = {
+  size: function(){
+    return this.length;
+  },
+
+  enqueue: function(value){
+    this.storage[this.length++] = value;
+  },
+
+  dequeue: function(){
+    this.length && this.length--;
+    var temp = this.storage[0];
+    delete this.storage[0];
+    for(var k in this.storage) {
+      var newKey = k-1;
+      if (newKey > -1) {
+        this.storage[newKey] = this.storage[k];
+      }
+    }
+    return temp;
+  }
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,33 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  //var instance = {};
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+
+  // instance.push = stackMethods.push;
+  // instance.size = stackMethods.size;
+  // instance.pop = stackMethods.pop;
+
+  return instance;
 };
+
+var stackMethods = {
+  size: function(){
+    return this.length;
+  },
+
+  push: function(value){
+    this.storage[this.length++] = value;
+  },
+
+  pop: function(){
+    this.length && this.length--;
+    return this.storage[this.length];
+  }
+};
+
