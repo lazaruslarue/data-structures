62661be9bf344ac7c84d4dc1018242c5c74bcdda
Hack Reactor Students <github@hackreactor.com>
2013-11-14 19:49:01 -0800

    addToTail and tests for first node addition

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -5,17 +5,35 @@ describe("linkedList", function() {
     linkedList = makeLinkedList();
   });
 
-
-
-
   it("should have a head and tail", function() {
     expect(Object.keys(linkedList)).toContain("head");
     expect(Object.keys(linkedList)).toContain("tail");
   });
-/* if it has nodes inside... then: */
-  it("should not have a 'null' head or tail (if there's anything inside it)", function() {
-    expect(linkedList.head).not.toEqual(null);
-    expect(linkedList.tail).not.toEqual(null);
+/* adding to tail: */
+  describe("adding to tail tests... ", function() {
+    beforeEach(function() {
+      linkedList.addToTail(13579);
+    });
+
+    afterEach(function() {
+      linkedList = makeLinkedList();
+    });
+
+    it("head & tail shouldn't be null after adding an element", function() {
+      expect(linkedList.head).not.toEqual(null);
+      expect(linkedList.tail).not.toEqual(null);
+    });
+
+    it("should have the same tail & head value after adding the first element to tail", function() {
+      expect(linkedList.head).toEqual(linkedList.tail);
+    });
+
+    it("should have a tail with value 13579 after addToTail on empty list", function() {
+      expect(linkedList.addToTail).toEqual(13579);
+    });
+
+
+
   });
 
   it("should have methods named 'addToTail', 'removeHead', and 'contains'", function() {
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,11 +1,25 @@
 // Note: don't use an array to do this.
 var makeLinkedList = function(){
   var list = {};
- 
+
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    if ( !this.head ) {
+      this.head = new makeNode(value);
+      this.tail = this.head;
+    } else {
+      this.tail.next = this.tail;
+      this.tail = new makeNode(value);
+    }
+    // does the list have a tail?
+    // if yes,
+    // if yes, make list.tail = 'value'
+    // else, list has no content, and make list.head = 'value'
+    // make list.tail = 'value' && list.head.next = 'value'
+    // does list have a head?
+    console.log(this.head, this.tail, this.tail.next);
 
   };
 
