3f28ed323db89f3b5a9d10e789cbb93d6b104eb7
Hack Reactor Students <github@hackreactor.com>
2013-11-15 21:04:41 -0800

    set complete & set test complete

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -3,11 +3,28 @@ describe("set", function() {
 
   beforeEach(function() {
     set = makeSet();
+    set.add('a');
+    set.add('b');
   });
 
   it("should have methods named 'add', 'contains', and 'remove'", function() {
     expect(set.add).toEqual(jasmine.any(Function));
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
+  });
+
+  it("should add a string to set", function() {
+    expect(set._storage['a']).toEqual(true);
+    expect(set._storage['b']).toEqual(true);
+  });
+  it("should search the set to find matching value", function() {
+    expect(set.contains('a')).toEqual(true);
+    expect(set.contains('b')).toEqual(true);
+    expect(set.contains('c')).toEqual(false);
+  });
+
+  it("should remove values when we say so", function() {
+    set.remove('a');
+    expect(set.contains('a')).toEqual(false);
   });
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -6,13 +6,21 @@ var makeSet = function(){
 
 var setPrototype = {};
 
-setPrototype.add = function(val){
-  this._storage[val] = Object.create(makeSet);
+setPrototype.add = function(key){
+  if (this._storage === undefined) {
+    this._storage = {};
+  }
+  this._storage[key] = true;
 };
 
-setPrototype.contains = function(){
-
+setPrototype.contains = function(nam){
+  if (this._storage[nam]) {
+    return this._storage[nam];
+  } else {
+    return false;
+  }
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(nam){
+  delete this._storage[nam];
 };
