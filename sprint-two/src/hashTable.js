f4ccd829d847f95f6f85a35fcea10f44235d251a
Hack Reactor Students <github@hackreactor.com>
2013-11-16 12:02:13 -0800

    we're so smart

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -14,17 +14,27 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
-  this._storage.set(i, v);
+  var result = [k, v];
+  this._storage.set(i, result);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  var storeIndex = this._storage.get(i);
+  // if (storeIndex.length > 1) 
+  for (var j = 0; j < storeIndex.length ; j++) {
+    if (storeIndex[j] === k) {
+      return storeIndex[j];
+    } else {
+      return "get outta here";
+    }
+  }
 };
 
 HashTable.prototype.remove = function(){
 };
+
+
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
